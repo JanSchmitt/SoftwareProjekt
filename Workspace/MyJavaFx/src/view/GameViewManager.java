b23017847bad2a157012fe7a25119053abbc3cef
Kerim Turacan <s180724@th-ab.de>
2020-04-22 13:32:05 +0200

    GHJ

--- Workspace/MyJavaFx/src/view/GameViewManager.java
+++ Workspace/MyJavaFx/src/view/GameViewManager.java
@@ -1,6 +1,8 @@
 package view;
 
 
+import java.util.Random;
+
 import javafx.animation.AnimationTimer;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
@@ -20,7 +22,7 @@ public class GameViewManager {
 	
 	private static final int GAME_WIDTH = 800;
 	private static final int GAME_HEIGHT = 750;
-	private static final int SHIP_SIZE = 60;
+	private static final int ENTITIES_SIZE = 60;
 	
 	private Stage menuStage;
 	private ImageView ship;
@@ -34,10 +36,17 @@ public class GameViewManager {
 	private GridPane gridPane2;
 	public final static String BACKGROUND_IMG = "view/resources/blue.png";
 	
+	private final static String METEOR_GREY_IMG = "view/resources/meteorGrey_med1.png";
+	private final static String METEOR_BROWN_IMG = "view/resources/meteorBrown_med1.png";
+	
+	private ImageView[] brownMeteors;
+	private ImageView[] greyMeteors;
+	Random RAND;
 	
 	public GameViewManager() {
 		initializeStage();
 		createKeyListener();
+		RAND = new Random();
 	}
 	
 	private void createKeyListener() {
@@ -80,21 +89,11 @@ public class GameViewManager {
 		this.menuStage.hide();
 		createBackground();
 		createShip();
+		createGameElements();
+		
 		createGameLoop();
 		gameStage.setTitle("Biofeedback Anwendung");
 		gameStage.show();
-	}
-	
-	private void createShip() {
-		
-		ship = new ImageView("view/resources/playerShip1_orange.png");
-		
-		ship.setFitHeight(SHIP_SIZE);
-		ship.setFitWidth(SHIP_SIZE);
-		
-		ship.setX(GAME_WIDTH / 2 - SHIP_SIZE / 2); //600/2-49 = 251  -49
-		ship.setY(GAME_HEIGHT - 90); //-90
-		gamePane.getChildren().add(ship);
 	}
 	
 	private void createGameLoop() {
@@ -102,23 +101,103 @@ public class GameViewManager {
 			@Override
 			public void handle(long now) {
 				moveBackground();
+				moveGameElements();
+				checkIfElemetsAreUnderShip();
 				moveShip();
 				
 			}
-			
 		};
 		gameTimer.start();
 	}
-	//99 x 75 breite x höhe
+	
+	
+	//Meteors
+	////////////////////////////////
+	private void createGameElements() {
+		//jeweils max 3 meteors generaten
+		brownMeteors = new ImageView[3];
+		for(int i = 0;i<brownMeteors.length; i++) {
+			//erzeugen der ImageView Objekte
+			brownMeteors[i] = new ImageView(METEOR_BROWN_IMG);
+			
+			//auf selbe Größe wie Player setzen
+			brownMeteors[i].setFitHeight(ENTITIES_SIZE);
+			brownMeteors[i].setFitWidth(ENTITIES_SIZE);
+			//
+			setNewElementPosition(brownMeteors[i]);
+			gamePane.getChildren().add(brownMeteors[i]);
+		}
+		
+		greyMeteors = new ImageView[3];
+		for(int i = 0;i<greyMeteors.length; i++) {
+			//auf selbe Größe wie Player setzen
+			greyMeteors[i] = new ImageView(METEOR_GREY_IMG);
+			
+			//auf selbe Größe wie Player setzen
+			greyMeteors[i].setFitHeight(ENTITIES_SIZE);
+			greyMeteors[i].setFitWidth(ENTITIES_SIZE);
+			//
+			setNewElementPosition(greyMeteors[i]);
+			gamePane.getChildren().add(greyMeteors[i]);
+		}
+	}
+	
+	private void setNewElementPosition(ImageView img) {
+		img.setLayoutX(RAND.nextInt(GAME_WIDTH - ENTITIES_SIZE));//370
+		//zahl beliebig
+		img.setLayoutY(-RAND.nextInt(400));
+	}
+	
+	private void moveGameElements() {
+		for(int i = 0; i < brownMeteors.length; i++) {
+			brownMeteors[i].setLayoutY(brownMeteors[i].getLayoutY() + 4);
+			brownMeteors[i].setRotate(brownMeteors[i].getRotate()+4);
+		}
+		
+		for(int i = 0; i < greyMeteors.length; i++) {
+			greyMeteors[i].setLayoutY(greyMeteors[i].getLayoutY() + 3); 
+			greyMeteors[i].setRotate(greyMeteors[i].getRotate()-4);
+		}
+	}
+	
+	private void checkIfElemetsAreUnderShip() {
+		for(int i = 0; i <  brownMeteors.length; i++) {
+			//wenn obj unter fensterrand
+			if(brownMeteors[i].getLayoutY() > GAME_HEIGHT) { //ship.getLayoutY()
+				setNewElementPosition(brownMeteors[i]);
+			}
+		}
+		
+		for(int i = 0; i <  greyMeteors.length; i++) {
+			//wenn obj unter fensterrand
+			if(greyMeteors[i].getLayoutY() > GAME_HEIGHT) {
+				setNewElementPosition(greyMeteors[i]);
+			}
+		}
+	}
+	
+	//Ship(Player)
+	///////////////////////////
+	private void createShip() {
+		
+		ship = new ImageView("view/resources/playerShip1_orange.png");
+		
+		ship.setFitHeight(ENTITIES_SIZE);
+		ship.setFitWidth(ENTITIES_SIZE);
+		
+		ship.setX(GAME_WIDTH / 2 - ENTITIES_SIZE / 2); //600/2-49 = 251  -49
+		ship.setY(GAME_HEIGHT - 90); //-90
+		gamePane.getChildren().add(ship);
+	}
+	
 	private void moveShip() {
 		//nach links
 		if(isLeftKeyPressed && !isRigtKeyPressed) {
-			System.out.println(ship.getLayoutX());
 			if(angle > -30) { //borders
 				angle -= 5;
 			}
 			ship.setRotate(angle);
-			if(ship.getLayoutX() > - GAME_WIDTH / 2 + SHIP_SIZE / 2) {
+			if(ship.getLayoutX() > - GAME_WIDTH / 2 + ENTITIES_SIZE / 2) {
 				ship.setLayoutX(ship.getLayoutX() -3);
 			}
 		}
@@ -129,7 +208,7 @@ public class GameViewManager {
 				angle += 5;
 			}
 			ship.setRotate(angle);
-			if(ship.getLayoutX() < GAME_WIDTH / 2 - SHIP_SIZE / 2) {
+			if(ship.getLayoutX() < GAME_WIDTH / 2 - ENTITIES_SIZE / 2) {
 				ship.setLayoutX(ship.getLayoutX() +3);
 			}
 		}
@@ -156,6 +235,8 @@ public class GameViewManager {
 		
 	}
 	
+	//Background
+	/////////////////////////////////
 	private void createBackground() {
 		//GridPane, da Background zu klein Screen-size ist
 		//Überlagerung des Backgrounds als Gittereinheit
