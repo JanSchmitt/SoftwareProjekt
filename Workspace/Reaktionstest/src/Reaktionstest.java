8f6c39d20afdf638329223b62b36d8062904157a
Jan Schmitt <s180667@th-ab.de>
2020-05-22 09:49:17 +0200

    Jan Sprint4 final

--- Workspace/Reaktionstest/src/Reaktionstest.java
+++ Workspace/Reaktionstest/src/Reaktionstest.java
@@ -6,6 +6,7 @@ import javafx.scene.input.KeyEvent;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
@@ -18,14 +19,16 @@ import java.io.File;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class Reaktionstest extends Application implements EventHandler<KeyEvent> {
+public class Reaktionstest extends Application {
 	Stage win;
 	Scene s, end;
 	Timeline timeline;
 	BorderPane b;
+	int counter;
+	Button button;
 	Label l1, l2, l3;
 	Circle c;
-	Timer timer1, timer2, timer3;
+	Timer timer1, timer2, timer3, timer;
 	TimerTask task;
 	int i = 0;
 	int points, V1, V2, V3;
@@ -39,49 +42,54 @@ public class Reaktionstest extends Application implements EventHandler<KeyEvent>
 	@Override
 	public void start(Stage reactio) {
 		win = reactio;
-		newScene();
-		
-		
-		
-		
-		
-		task = new TimerTask() {
-			@Override
-			public void run() {
-				checkForKey();
-				if(key == true) {
-					timer1.cancel();
-					//task.cancel();
-					System.out.println("Hier");
-					i++;
-					time();
-				} else {
-					points = points - 1;
-				}	
-			}
-		};
+		win.setTitle("Reaktionstest");
+		b = new BorderPane();
+		c = new Circle(100);
+		c.setStroke(Color.BLACK);
+		c.setFill(Color.WHITE);
+		b.setCenter(c);
+		s = new Scene(b, 400, 400);
+		win.setScene(s);
+		win.show();
+		i = 1;
+		time();
 	}
 	
-	public void checkForKey() {
-		key  = false;
-		new EventHandler<KeyEvent>() {
-			@Override
-			public void handle(KeyEvent event) {
-				if ((event.getCode() == KeyCode.ENTER) && (i <= 3)) {
-					if (i == 1) {
-						System.out.println("Hier");
-						V1 = points;
-					} else if (i == 2) {
-						V2 = points;
-					} else if (i == 3) {
-						V3 = points;
-					}
-					key = true;
-				} else {
-					key = false;
+	public void newScene() {
+		b = new BorderPane();
+		b.setCenter(c);
+		s = new Scene(b, 400, 400);
+		if (i == 1) {
+			c.setFill(Color.GREEN);
+			System.out.println("Scene1");
+			points = 1000;
+			createTimer();
+			win.setScene(s);
+		} else if (i == 2) {
+			playMusic(Beep);
+			System.out.println("Scene2");
+			points = 1000;
+			createTimer();
+		} else if (i == 3) {
+			playMusic(Beep);
+			c.setFill(Color.BLUE);
+			System.out.println("Scene2");
+			points = 1000;
+			createTimer();
+			win.setScene(s);
+		} else if (i == 4) {
+			VBox v = new VBox(8);
+			v.getChildren().addAll(l1, l2, l3);
+			end = new Scene(v, 400, 400);
+			timeline = new Timeline(new KeyFrame(Duration.seconds(5), new EventHandler<ActionEvent>() {
+				@Override
+				public void handle(ActionEvent e) {
+					timeline.stop();
+					win.close();
 				}
-			}
-		};
+			}));
+			timeline.play();	
+		}
 	}
 
 	public void time() {
@@ -93,47 +101,70 @@ public class Reaktionstest extends Application implements EventHandler<KeyEvent>
 			}
 		}));
 		timeline.play();
-	};
+	}
 
-	public void newScene() {
-		if (i == 0) {
-			win.setTitle("Reaktionstest");
-			b = new BorderPane();
-			c = new Circle(100);
-			c.setStroke(Color.BLACK);
-			c.setFill(Color.WHITE);
-			b.setCenter(c);
-			s = new Scene(b, 400, 400);
-			win.setScene(s);
-			win.show();
-			i++;
-			time();
-		} else if (i == 1) {
-			c.setFill(Color.GREEN);
-			timer1 = new Timer();
-			System.out.println("Hier");
-			points = 1000;
-			timer1.scheduleAtFixedRate(task, 1, 1);
-			win.setScene(s);
-		} else if (i == 2) {
-			playMusic(Beep);
-			timer2 = new Timer();
-			points = 1000;
-			timer2.scheduleAtFixedRate(task, 1, 1);
-		} else if (i == 3) {
-			playMusic(Beep);
-			c.setFill(Color.BLUE);
-			timer3 = new Timer();
-			points = 1000;
-			timer3.scheduleAtFixedRate(task, 1, 1);
-		} else if (i == 4) {
-			timeline.stop();
-			VBox v = new VBox(8);
-			v.getChildren().addAll(l1, l2, l3);
-			end = new Scene(v, 400, 400);
-		} else if (i == 5) {
-			win.close();
-		}
+	public void createTimer() {
+
+		timer = new Timer();
+		task = new TimerTask() {
+			@Override
+			public void run() {
+				/*button.setOnKeyPressed(new EventHandler<KeyEvent>() {
+					@Override
+					public void handle(KeyEvent ke) {
+						if(ke.getCode() == KeyCode.ENTER) {
+							key = true;
+						} else {
+							key = false;
+						}
+					}	
+				});*/
+				checkForKey();
+				if (key == true) {
+					timer.cancel();
+					System.out.println("Timer" + i + " gecancelt");
+					i++;
+					time();
+				} else {
+					points = points - 1;
+				}
+				if(points <= -1000) {
+					timer.cancel();
+					System.out.println("Timer" + i + " gecancelt");
+					i++;
+					time();
+				}
+			}
+		};
+		timer.scheduleAtFixedRate(task, 1, 1);
+	}
+
+	public void checkForKey() {
+		key = false;
+		new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent event) {
+
+				if (event.getCode() == KeyCode.ENTER) {
+					if (i == 1) {
+						System.out.println("Punkte 1");
+						V1 = points;
+					} else if (i == 2) {
+						System.out.println("Punkte 2");
+						V2 = points;
+					} else if (i == 3) {
+						System.out.println("Punkte 3");
+						V3 = points;
+					}
+					key = true;
+					System.out.println("Enter");
+				} else {
+					key = false;
+				}
+
+			}
+		};
+
 	}
 
 	public static void playMusic(File sound) {
@@ -147,24 +178,4 @@ public class Reaktionstest extends Application implements EventHandler<KeyEvent>
 			System.out.println("Fehler");
 		}
 	}
-
-	@Override
-	public void handle(KeyEvent event) {
-
-		if ((event.getCode() == KeyCode.ENTER) && (i <= 3)) {
-			if (i == 1) {
-				timer1.cancel();
-				System.out.println("Hier");
-				V1 = points;
-
-			} else if (i == 2) {
-				V2 = points;
-			} else if (i == 3) {
-				V3 = points;
-			}
-		}
-		i++;
-		time();
-	}
-
 }
