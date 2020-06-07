8a1c30ce41f8b6da395b99be95c94e29b235f5c9
Tim Schmitt <s180670@th-ab.de>
2020-06-07 18:33:14 +0200

    Testfunktionen hinzugefügt

--- Workspace/HRS/src/Port.java
+++ Workspace/HRS/src/Port.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
-
 import com.fazecast.jSerialComm.SerialPort;
 
 public class Port {
 
-	public static void main(String[] args) {
+	public void rec(int c) {
+		int r = c;
+		int curr;
+		int sum = 0;
 		SerialPort ports[] = SerialPort.getCommPorts();
 		System.out.println("Select a port");
 		int i = 1;
@@ -24,8 +26,31 @@ public class Port {
 		s.close();
 		Scanner data = new Scanner(port.getInputStream());
 		while(data.hasNextLine()) {
-			System.out.println(data.nextLine());
+			curr = data.nextInt();
+			System.out.println(curr);
+			sum = sum + curr;
+			if(r > 1) {
+				r = r-1;
+			}else if(r == 1) {
+				break;
+			}else if(r == 0) {
+				
+			}
+		}
+		if(c != 0) {
+			System.out.println("Mittlere Herzrate = " + sum/c);
 		}
 	}
-
+	
+	
+	
+	public void start() {
+		int time = 0;
+		rec(time);
+	}
+	
+	public void test(){
+		int f = 10;
+		rec(f);
+	}
 }
